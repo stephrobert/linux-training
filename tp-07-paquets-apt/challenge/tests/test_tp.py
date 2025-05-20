8f75125c3f6038ce4dfd2debf9821eb32a497118
Stéphane ROBERT <robert.stephane.28@gmail.com>
2025-05-20 08:09:16 +0000

    fix: renomage

--- tp-07-paquets-apt/challenge/tests/test_tp.py
+++ tp-07-paquets-apt/challenge/tests/test_tp.py
@@ -9,10 +9,10 @@ def test_preferences(host):
     preferences = host.file("/etc/apt/preferences")
     assert preferences.exists
     assert preferences.contains("Package: nano")
-    assert preferences.contains("Pin: version 2.9.8-1")
+    assert preferences.contains("Pin: version 7.2")
     assert preferences.contains("Pin-Priority: 1001")
 
 def test_nano_version(host):
     nano = host.package("nano")
     assert nano.is_installed
-    assert nano.version == "2.9.8-1"
+    assert nano.version == "7.2"
